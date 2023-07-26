d0ce0cc80006b6a106bf44c1f21f3b498a7f537d
ataayyıldız <84992313+satas20@users.noreply.github.com>
2023-07-27 00:44:56 +0300

    SpawnManager

--- UDO/Assets/Scripts/Managers/SpawnManager.cs
+++ UDO/Assets/Scripts/Managers/SpawnManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+ 
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _burgerPrefab;
+
+    [Space(50)]
+    [SerializeField] private float _spawnRate;
+    [Range(2,6)]
+    [SerializeField] private float _jumpLength;
+    [SerializeField] private float _jumpPower;
+    [SerializeField] private float _jumpDuration;
+
+
+    bool isSpawn = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Spawn(_spawnRate));
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public IEnumerator Spawn(float time){
+        while (isSpawn)
+        {
+
+            Vector3 spawnPos  = new Vector3(Random.Range(-100f,100f), 0, Random.Range(-100f, 100f)).normalized*2;
+            GameObject newBurger= Instantiate(_burgerPrefab, spawnPos, Quaternion.identity);
+            Vector3 jumpPos = transform.position+(spawnPos - transform.position).normalized* (_jumpLength+Random.Range(-1f,1f));
+            jumpPos.y += 1;
+            
+
+            newBurger.transform.DOJump(jumpPos,_jumpPower,1, _jumpDuration).SetEase(Ease.OutQuad);
+            yield return new WaitForSeconds(time);
+
+        }
     }
 }
