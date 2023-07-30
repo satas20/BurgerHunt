c1b3cbdb77a0dcd3de8d9165fcc4acef86f18d1a
ataayyıldız <84992313+satas20@users.noreply.github.com>
2023-07-30 14:41:48 +0300

    Revert "Adding Assets"
    This reverts commit d659bf7995a47f6e686f1af037882f3360b95192.

--- UDO/Assets/Scripts/Managers/SpawnManager.cs
+++ UDO/Assets/Scripts/Managers/SpawnManager.cs
@@ -15,25 +15,19 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private float _jumpLength;
     [SerializeField] private float _jumpPower;
     [SerializeField] private float _jumpDuration;
-    private GameObject player;
+
 
     bool isSpawn = true;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
         StartCoroutine(Spawn(_spawnRate));
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rotate();
-    }
-    private void Rotate()
-    {
-        var dir = player.transform.position-transform.position;
-        transform.rotation = Quaternion.LookRotation(dir, Vector3.up);
+       
     }
     public IEnumerator Spawn(float time){
         while (isSpawn)
@@ -57,21 +51,14 @@ public class SpawnManager : MonoBehaviour
      public void spawnGold(int amount) {
         for(int i = 0; i < amount; i++){
             Debug.Log("spawned");
-            Quaternion target = Quaternion.Euler(0, Random.Range(-180f,180f),0);
             Vector3 spawnPos = new Vector3(Random.Range(-100f, 100f), 0, Random.Range(-100f, 100f)).normalized * 2;
-            GameObject newCoin = Instantiate(_coinPrefab, spawnPos, target);
-            Vector3 jumpPos = transform.position + (spawnPos - transform.position).normalized * (6+Random.Range(-2f, 4f));
+            GameObject newCoin = Instantiate(_coinPrefab, spawnPos, Quaternion.identity);
+            Vector3 jumpPos = transform.position + (spawnPos - transform.position).normalized * (3 + Random.Range(-1f, 1f));
             jumpPos.y += 1;
 
 
-            newCoin.transform.DOJump(jumpPos, 7, 1, 0.7f).SetEase(Ease.OutQuad);
+            newCoin.transform.DOJump(jumpPos, 4, 1, _jumpDuration).SetEase(Ease.OutQuad);
             
-        }
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Food")){
-            Destroy(other.gameObject);
         }
     }
 }
